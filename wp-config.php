0474e7bf101a61618781e0231d539fa205b42cc9
phillips lamp <phillips.lamp42@gmail.com>
2022-01-02 23:10:43 +0700

    update: penyesuaian db name di hosting

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wp_pisbagia' );
+define( 'DB_NAME', 'pisj8776_wp_pisbagia' );
 
 /** MySQL database username */
 define( 'DB_USER', 'pisj8776' );
