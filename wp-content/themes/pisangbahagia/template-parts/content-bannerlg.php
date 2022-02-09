73e85b79b56bd92b21327c588e29a2073d1c316b
MATISSE\Matisse <phillips.lamp42@gmail.com>
2022-02-09 15:33:55 +0700

    Update :  Menambahkan kategori pada banner-lg dan merubahbanner-sm title nya ke bawah

--- wp-content/themes/pisangbahagia/template-parts/content-bannerlg.php
+++ wp-content/themes/pisangbahagia/template-parts/content-bannerlg.php
@@ -1,6 +1,11 @@
-        <div class="banner-content ">
+        <div class="banner-content">
             <a href="<?=the_permalink();?>" class="bg-dark">
                 <div class="ft banner-ctn lg">
+                    <div class="banner-cat-wrap">
+                        <?php foreach( get_the_category( get_post()->ID ) as $category ):?>
+                        <label class="ft-po"><?=$category->name;?></label>
+                        <?php endforeach; ?>
+                    </div>
                     <p class="m-0"><?=the_title();?></p>
                     <small class="ft-po m-0 ft-b-4 ft-sz-13 cl-ligrey">
                         <?=get_the_author_meta( 'user_firstname', $post->post_author );?>,&nbsp;
--- wp-content/themes/pisangbahagia/template-parts/content-bannersm.php
+++ wp-content/themes/pisangbahagia/template-parts/content-bannersm.php
@@ -1,6 +1,12 @@
         <div class="banner-content-half anim-loader-skeleton">
             <a href="<?=the_permalink();?>">
-                <p class="ft banner-ctn sm"><?=the_title();?></p>
+                <div class="ft banner-ctn sm">
+                    <p class="ft my-sm-1"><?=the_title();?></p>
+                    <small class="ft-po m-0 ft-b-4 ft-sz-12 cl-ligrey">
+                        <?=get_the_author_meta( 'user_firstname', $post->post_author );?>,&nbsp;
+                        <?=get_the_date( 'd F Y' );?>
+                    </small>
+                </div>
                 <img class="banner-img-sm" src="<?=get_the_post_thumbnail_url();?>">
             </a>
         </div>
